9d9f231f9d91b7935a6f52552179be0115897913
zonk2world <zonk2world@gmail.com>
2024-08-07 07:31:37 -0400

    initial commit

---
+++ docs.nvidia.com/deeplearning/cudnn/archives/cudnn-897/search/htmlFileInfoList.js
@@ -0,0 +1,8 @@
+fil = new Array();
+fil["0"]= "api/index.html@@@API Reference :: NVIDIA cuDNN Documentation@@@This use case describes the steps with which users set up an execution plan with a previously finalized engine config descriptor, set up the data pointer variant pack, and finally execute the plan...";
+fil["1"]= "archives/index.html@@@Documentation Archives :: NVIDIA cuDNN Documentation@@@These are the cuDNN v7 archived documents...";
+fil["2"]= "developer-guide/index.html@@@Developer Guide :: NVIDIA cuDNN Documentation@@@Some of the cuDNN library routines were derived from code developed by others and are subject to the following...";
+fil["3"]= "install-guide/index.html@@@Installation Guide :: NVIDIA cuDNN Documentation@@@NVIDIA would like to thank the following individuals and institutions for their contributions...";
+fil["4"]= "release-notes/index.html@@@Release Notes :: NVIDIA cuDNN Documentation@@@This is the cuDNN 7.0.1 release notes. This release includes the following changes...";
+fil["5"]= "sla/index.html@@@Software License Agreement :: NVIDIA cuDNN Documentation@@@The terms in this supplement govern your use of the NVIDIA cuDNN SDK under the terms of your license agreement (“Agreement”) as modified by this supplement. Capitalized terms used but not defined below have the meaning assigned to them in the Agreement...";
+fil["6"]= "support-matrix/index.html@@@Support Matrix :: NVIDIA cuDNN Documentation@@@Windows 10 and Windows Server 2019 and 2016 are supported. Refer to the following table to view the list of supported Visual Studio versions for cuDNN...";
---
+++ docs.nvidia.com/deeplearning/cudnn/archives/cudnn-897/search/htmlFileList.js
@@ -0,0 +1,10 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "api/index.html";
+fl["1"]= "archives/index.html";
+fl["2"]= "developer-guide/index.html";
+fl["3"]= "install-guide/index.html";
+fl["4"]= "release-notes/index.html";
+fl["5"]= "sla/index.html";
+fl["6"]= "support-matrix/index.html";
+var doStem = true
